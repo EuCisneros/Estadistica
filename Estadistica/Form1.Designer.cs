07d1f4b1babbd8f27286c3bf7564edd02ed45ecc
EuCisneros <eucisnerosyt@gmail.com>
2020-02-12 14:22:47 -0600

    estandar, tipica

--- Estadistica/Form1.Designer.cs
+++ Estadistica/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.btnmedia = new System.Windows.Forms.Button();
             this.lblserie = new System.Windows.Forms.Label();
-            this.lblrespuestaserie = new System.Windows.Forms.Label();
+            this.lblrespuesta = new System.Windows.Forms.Label();
             this.txtserie = new System.Windows.Forms.TextBox();
             this.btnestandar = new System.Windows.Forms.Button();
             this.btntipica = new System.Windows.Forms.Button();
@@ -38,7 +38,7 @@
             // 
             // btnmedia
             // 
-            this.btnmedia.Location = new System.Drawing.Point(50, 136);
+            this.btnmedia.Location = new System.Drawing.Point(21, 136);
             this.btnmedia.Name = "btnmedia";
             this.btnmedia.Size = new System.Drawing.Size(75, 23);
             this.btnmedia.TabIndex = 0;
@@ -49,55 +49,57 @@
             // lblserie
             // 
             this.lblserie.AutoSize = true;
-            this.lblserie.Location = new System.Drawing.Point(47, 53);
+            this.lblserie.Location = new System.Drawing.Point(18, 53);
             this.lblserie.Name = "lblserie";
             this.lblserie.Size = new System.Drawing.Size(42, 13);
             this.lblserie.TabIndex = 1;
             this.lblserie.Text = "SERIE:";
             // 
-            // lblrespuestaserie
+            // lblrespuesta
             // 
-            this.lblrespuestaserie.AutoSize = true;
-            this.lblrespuestaserie.Location = new System.Drawing.Point(47, 89);
-            this.lblrespuestaserie.Name = "lblrespuestaserie";
-            this.lblrespuestaserie.Size = new System.Drawing.Size(14, 13);
-            this.lblrespuestaserie.TabIndex = 2;
-            this.lblrespuestaserie.Text = "X";
+            this.lblrespuesta.AutoSize = true;
+            this.lblrespuesta.Location = new System.Drawing.Point(23, 89);
+            this.lblrespuesta.Name = "lblrespuesta";
+            this.lblrespuesta.Size = new System.Drawing.Size(14, 13);
+            this.lblrespuesta.TabIndex = 2;
+            this.lblrespuesta.Text = "X";
             // 
             // txtserie
             // 
-            this.txtserie.Location = new System.Drawing.Point(95, 46);
+            this.txtserie.Location = new System.Drawing.Point(66, 47);
             this.txtserie.Name = "txtserie";
             this.txtserie.Size = new System.Drawing.Size(197, 20);
             this.txtserie.TabIndex = 3;
             // 
             // btnestandar
             // 
-            this.btnestandar.Location = new System.Drawing.Point(143, 136);
+            this.btnestandar.Location = new System.Drawing.Point(117, 136);
             this.btnestandar.Name = "btnestandar";
             this.btnestandar.Size = new System.Drawing.Size(75, 23);
             this.btnestandar.TabIndex = 4;
             this.btnestandar.Text = "D_Estandar";
             this.btnestandar.UseVisualStyleBackColor = true;
+            this.btnestandar.Click += new System.EventHandler(this.btnestandar_Click);
             // 
             // btntipica
             // 
-            this.btntipica.Location = new System.Drawing.Point(240, 136);
+            this.btntipica.Location = new System.Drawing.Point(212, 136);
             this.btntipica.Name = "btntipica";
             this.btntipica.Size = new System.Drawing.Size(75, 23);
             this.btntipica.TabIndex = 5;
             this.btntipica.Text = "D_Tipica";
             this.btntipica.UseVisualStyleBackColor = true;
+            this.btntipica.Click += new System.EventHandler(this.btntipica_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(352, 206);
+            this.ClientSize = new System.Drawing.Size(310, 211);
             this.Controls.Add(this.btntipica);
             this.Controls.Add(this.btnestandar);
             this.Controls.Add(this.txtserie);
-            this.Controls.Add(this.lblrespuestaserie);
+            this.Controls.Add(this.lblrespuesta);
             this.Controls.Add(this.lblserie);
             this.Controls.Add(this.btnmedia);
             this.Name = "Form1";
@@ -111,7 +113,7 @@
 
         private System.Windows.Forms.Button btnmedia;
         private System.Windows.Forms.Label lblserie;
-        private System.Windows.Forms.Label lblrespuestaserie;
+        private System.Windows.Forms.Label lblrespuesta;
         private System.Windows.Forms.TextBox txtserie;
         private System.Windows.Forms.Button btnestandar;
         private System.Windows.Forms.Button btntipica;
--- Estadistica/Form1.cs
+++ Estadistica/Form1.cs
@@ -21,8 +21,18 @@ namespace Estadistica
         private void btnmedia_Click(object sender, EventArgs e)
         {
             //split divide una cadema en base a un delimitador (patron) y me devuelve una matriz
-            lblrespuestaserie.Text = "X = " +  objEstadistica.media(txtserie.Text.Split(','));
+            lblrespuesta.Text = "X = " +  objEstadistica.media(txtserie.Text.Split(','));
 
+        }
+
+        private void btnestandar_Click(object sender, EventArgs e)
+        {
+            lblrespuesta.Text = "Estandar = " + objEstadistica.estandar(txtserie.Text.Split(','));
+        }
+
+        private void btntipica_Click(object sender, EventArgs e)
+        {
+            lblrespuesta.Text = "Tipica = " + objEstadistica.tipica(txtserie.Text.Split(','));
         }
     }
 }
